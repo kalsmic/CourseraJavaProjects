d1a0feb4d82f241879826c166e428896356d187a
Arthur Kalule <kalulearthur@gmail.com>
2021-05-23 17:33:06 +0300

    Use explicit scoping instead of the default package private level

--- src/test/java/company/ciphers/CaesarBreakerTest.java
+++ src/test/java/company/ciphers/CaesarBreakerTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CaesarBreakerTest {
-    CaesarBreaker cb = new CaesarBreaker();
+    private CaesarBreaker cb = new CaesarBreaker();
 
 
     @Test
