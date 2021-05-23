af499cffcaf0a0208813229ea474bde5cbe3621d
Arthur Kalule <kalulearthur@gmail.com>
2021-05-23 17:38:36 +0300

    Avoid unused private fields

--- src/test/java/company/ciphers/VigenereCipherTest.java
+++ src/test/java/company/ciphers/VigenereCipherTest.java
@@ -3,16 +3,23 @@ package test.java.company.ciphers;
 import edu.duke.FileResource;
 import main.java.company.ciphers.VigenereCipher;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class VigenereCipherTest {
-    private int[] key = {17, 14, 12, 4};
+    private int[] key = new int[]{17, 14, 12, 4};
     private VigenereCipher vc;
     private String testMessage = new FileResource("src/main/resources/data/titus-small.txt").asString();
     private String testEncryptedMessage = new FileResource("src/main/resources/data/titus-small-key17-14-12-4.txt").asString();
 
+
+    @BeforeEach
+    void setUp() {
+        vc = new VigenereCipher(key);
+        ;
+    }
 
     @AfterEach
     void tearDown() {
@@ -21,23 +28,19 @@ class VigenereCipherTest {
 
     @Test
     void encrypt() {
-        VigenereCipher vc = new VigenereCipher(new int[]{17, 14, 12, 4});
         String encrypted = vc.encrypt(testMessage);
         assertEquals(testEncryptedMessage, encrypted);
     }
 
     @Test
     void decrypt() {
-        VigenereCipher vc = new VigenereCipher(new int[]{17, 14, 12, 4});
         String decrypted = vc.decrypt(testEncryptedMessage);
         assertEquals(testMessage, decrypted);
 
     }
 
-
     @Test
     void testToString() {
-        VigenereCipher vc = new VigenereCipher(new int[]{17, 14, 12, 4});
         assertEquals("[17, 14, 12, 4]", vc.toString());
     }
 }
